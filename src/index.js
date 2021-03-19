d26c62d795046802af96ef20ff68f3c4d1d6077c
Tatiana Perez <tjperez@ncsu.edu>
2021-03-19 15:07:16 -0400

    showing more but on every one at once

--- src/index.js
+++ src/index.js
@@ -1,32 +1,60 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import data from './data.js' 
+import './app.css';
 
-
-console.log(data)
 
 const App = () => {
-  const [books] = useState(data)
+  const [books, setBook] = useState(data)
+  const [isLess, setisMore] = useState(false)
+  const onClick = () => setisMore(!isLess)
+  console.log(isLess)
   return (
     <main>
-      <h1>Programming books</h1>
+      <h1>Free Shelf Thing</h1>
       {books.map((book) => (
-        <Book title={book.title} author={book.author} description={book.shortDescription}/>
+        <div>
+        <Book isLess={isLess} image={book.coverImageUrl} title={book.title} author={book.author} description={book.shortDescription} url={book.url} publisher={book.publisher} publicationDate={book.publicationDate} detailedDescription={book.detailedDescription}/>
+        <button onClick={onClick}>abc</button>
+        </div>
       ))}
     </main>
   )
 }
 
 const Book = (props) => {
-  const { title, author, description } = props
+  const { isLess, image, title, author, description, url, publisher, publicationDate, detailedDescription} = props
   return (
     <div className="books">
-      <h2>{title}</h2>
-      <div>{author}</div>
-      <div>{description}</div>
+      <img src={image}></img>
+      <div>
+        <h2>{title}</h2> 
+        <h3>{author}</h3> 
+        <p>{description}</p>
+        <div style={isLess ? {} : { display: 'none' }}>
+        <div>{url}</div> 
+        <div>{publisher} {publicationDate}</div>
+        <p>{detailedDescription}</p>
+        </div>
+      </div>
+      
     </div>
   )
 }
+
+// const MoreBtn = (props) => {
+//   return (
+//     <button onClick={HandleClick}>abc</button>
+//   )
+// }
+
+// const HandleClick = () => {
+//   setBook("help")
+// }
+
+
+
+
 
 
   ReactDOM.render(<App />, document.querySelector('#root'))
